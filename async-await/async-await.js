22ba1fbba319582a1c3f76b2c06c6b71d0ec778d
Clark <montebonclarkjasper7@gmail.com>
2024-10-27 21:52:36 +0800

    feat: added async-await folder

---
+++ async-await/async-await.js
@@ -0,0 +1,77 @@
+
+
+const preHeatOven = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const preHeatOven = true;
+
+            if(preHeatOven) {
+                resolve("Preheat oven to 180deg.");
+            } else {
+                reject("Task failed!")
+            }
+        }, 1000)
+    })
+}
+
+
+const addSugarAndChocoChips = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const addChoco = true;
+
+            if(addChoco) {
+                resolve("Place butter and chocolcate chips, stir until melted and smooth.");
+            } else {
+                reject("Task failed!")
+            }
+        }, 1000)
+    })
+}
+
+const addFlourCocoaAndSalt = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const addSaltFlour = true;
+
+            if(addSaltFlour) {
+                resolve("Add flour, cocoa and salt and stir until smooth.");
+            } else {
+                reject("Task failed!")
+            }
+        }, 1000)
+    })
+}
+
+const bakeMixture = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const bakeMixture = true;
+
+            if(bakeMixture) {
+                resolve("Bake for 24 minutes.");
+            } else {
+                reject("Task failed!")
+            }
+        }, 1000)
+    })
+}
+
+const bakeChocolateBrownies = async () => {
+    const task1 = await preHeatOven();
+    console.log(task1);
+
+    const task2 = await addSugarAndChocoChips();
+    console.log(task2);
+
+    const task3 = await addFlourCocoaAndSalt();
+    console.log(task3);
+
+    const task4 = await bakeMixture();
+    console.log(task4);
+
+    console.log("Enjoy your Brownies!")
+
+}
+
+bakeChocolateBrownies();

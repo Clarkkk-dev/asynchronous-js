717eb22d7c7985162c561ac907c18fc9ddcda760
Clark <montebonclarkjasper7@gmail.com>
2024-10-28 09:44:11 +0800

    feat: added chuck-norris-api folder

---
+++ chuck-noris-api/app.js
@@ -0,0 +1,26 @@
+
+const jokeElement = document.getElementById('loadingJoke');
+const loadJokeBtn = document.getElementById('loadJokeBtn');
+
+
+const loadJoke = async () => {
+    
+    try{
+
+        const fetchJokes = await fetch('https://api.chucknorris.io/jokes/random',
+            {
+                headers: {
+                    Accept: "application/json"
+                }
+            }
+
+        );
+        const jokeData = await fetchJokes.json();
+
+        jokeElement.innerHTML = jokeData.value;
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+loadJokeBtn.addEventListener("click", loadJoke);
